b27b0b3922b9f375a57793e308b8a42663e641f9
nicktming <2279563278@qq.com>
2018-12-15 20:24:38 +0800

    add tensorflow

---
+++ tensorflow_deep_learning/chapter03/tensorflow_02.py
@@ -0,0 +1,12 @@
+import tensorflow as tf 
+
+a = tf.constant([1, 2], name="a", dtype=tf.float32)
+b = tf.constant([2.0, 3.0], name="b")
+
+result = tf.add(a, b, name="add")
+
+#print (result.get_shape)
+
+print result
+
+
