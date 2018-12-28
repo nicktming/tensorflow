413826693fa71ee3074017d21aa22455d15ab8cb
nicktming <2279563278@qq.com>
2018-12-28 21:27:19 +0800

    add 3_1_2_02

--- tensorflow_goole_in_action/chapter03/3_1_2_01.py
+++ tensorflow_goole_in_action/chapter03/3_1_2_01.py
@@ -4,3 +4,9 @@ a = tf.constant([1.0, 2.0], name="a")
 b = tf.constant([2.0, 3.0], name="b")
 
 result = a + b
+
+print(a.graph)
+print(b.graph)
+print(tf.get_default_graph())
+
+print(a.graph == b.graph)
